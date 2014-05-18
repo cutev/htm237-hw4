380c8b363b6aff5293e6bb555daedb5a202c44c0
cutev <cutev1980@gmail.com>
2014-05-18 23:58:45 +0800

    htm237-hw4 06
    hw4 06

--- js/checker.js
+++ js/checker.js
@@ -8,17 +8,12 @@ var junkGroups = [];
 
 // 用 Ajax 自 http://spamgroup.tonyq.org/groups/jsonp 取得垃圾社團列表
 
-$.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
-  dataType: 'jsonp',
-  jsonp: 'jsonp',
-  success: function(data){
-    // 將每筆資料的 GID 放進 junkGroups 陣列中。
-    var i;
-    for(i=0; i<data.length; i+=1){
-    junkGroups.push(data[i].GID);  
-    }
-    startButton.removeAttr('disabled').removeClass('disabled');
-  }
+$.getJSON("http://jsbin.com/jaziroja/1", {}, function(data){
+  // 將每筆資料的 GID 放進 junkGroups 陣列中。
+  data.forEach(function(record){
+    junkGroups.push(record.GID);
+  });
+  startButton.removeAttr('disabled').removeClass('disabled');
 });
 
 // 設定 Facebook AppID
@@ -28,28 +23,22 @@ window.fbAsyncInit = function(){
     status: true
   });
 
-  // 比對每個使用者的 group 是否有在 junkGroups 中出現
-  //
+  
   startButton.click(function(){
-    results.empty(); // 清除結果內容
-    $('.hw4-complete').remove(); // 移除「掃描完成」
-
-    // 1. 讓使用者登入此 Facebook App (FB.login)
-    FB.login(function(){
+    results.empty(); 
+    $('.hw4-complete').remove(); 
+       FB.login(function(){
       console.log('Logged in!');
-      FB.api('me/groups', function(){
-      console.log('user_groups', resp.data);
-      var i, group;
-      for(i=0; i<resp.data.length; i+=1){
-        group = resp.data[i];
-        if(junkGroups.indexOf(group.id)!== -1){
-          results.append('<tr><td>'+group.id+'</td><td>'+group.name+'</td></tr>');
+    FB.api('/me/groups', function(resp){
+      console.log('user-groups:!, resp.data');
+        var i;
+        for(i=0; i<resp.data.length; i+=1){
+          if( junkGroups.indexOf( resp.data[i].id ) !== -1 ){
+            results.append('<tr><td>'+resp.data[i].id+'</td><td>'+resp.data[i].name+'</td></tr>');
+          }
         }
-      }
-    // 2. 以 FB.api 拿到使用者的 group 列表
-    // 拿到使用者 group 列表的 response 之後：
-    results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+        results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
       });
-    },{scope: 'user_groups'});
+    }, {scope: 'user_groups'});
   });
 };
