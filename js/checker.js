7b2255c5c47ddf33d8f705600f32ddc4a08d9e46
cutev <cutev1980@gmail.com>
2014-05-18 23:28:10 +0800

    htm273-hw401
    htm273-hw401

--- js/checker.js
+++ js/checker.js
@@ -13,11 +13,10 @@ $.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
   jsonp: 'jsonp',
   success: function(data){
     // 將每筆資料的 GID 放進 junkGroups 陣列中。
-    //
-    // ...
-    //   junkGroups.push(data[i].GID);
-    // ...
-    //
+    var i;
+    for(i=0; i<data.length; i+=1){
+    junkGroups.push(data[i].GID);  
+    }
     startButton.removeAttr('disabled').removeClass('disabled');
   }
 });
@@ -25,7 +24,7 @@ $.ajax('http://spamgroup.tonyq.org/groups/jsonp', {
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
-    appId: '259195264246285', // 若可以，請換成自己的 App ID !
+    appId: '608059295955951', // 若可以，請換成自己的 App ID !
     status: true
   });
 
@@ -36,9 +35,21 @@ window.fbAsyncInit = function(){
     $('.hw4-complete').remove(); // 移除「掃描完成」
 
     // 1. 讓使用者登入此 Facebook App (FB.login)
+    FB.login(function)(){
+      console.log('Logged in!');
+      FB.api('me/groups', function(){
+      console.log('user_groups', resp.data);
+      var i, group;
+      for(i=0; i<resp.data.length; i+=1){
+        group = resp.data[i];
+        if(junkGroups.indexOf(group.id)!== -1){
+          results.append('<tr><td>'+group.id+'</td><tr>'group.name+'</td></tr>');
+        }
+      }
     // 2. 以 FB.api 拿到使用者的 group 列表
     // 拿到使用者 group 列表的 response 之後：
-    // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
-
+    results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+      });
+    },{scope: 'user_groups'});
   });
 };
